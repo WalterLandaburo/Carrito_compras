a76296f32205fe73dac618ba3a79f3e442cbdffd
WalterLandaburo <walter.libreria@gmail.com>
2025-06-30 14:01:57 -0300

    README.md
    Python
    Gestión de productos progresando a un carrito de compras 🛒 
    
    Aplicar módulo estándar datetime para mostrar fecha y hora de la acción realizada por el usuario/a.
    
    Aplicar docstring en las funciones
    
    Aplicar funciones para descomponer en partes 
    
    Aplicar try-except para manejar errores con los datos ingresados
    
    Aplicar importar módulo estándar datetime al agregar un producto al carrito 🛒 
    
    Usar una función para calcular la cantidad del producto elegido por su precio 
    
    Usar una función para mostrar el total a pagar 
    
    
    # Falla al calcular el total a pagar. No logro sumar los precios obtenidos.
    El plan era usar una función que sume todo el contenido de una sub-lista con los precios de los productos del carrito 🛒.
    La otra opción intentada fue un bucle for con un acumulador.
    Ambas opciones intentos fracasaron.
    
    
    #Lista principal donde se almacenan los productos

---
+++ Carrito_compras_list_sublist.py
@@ -0,0 +1,248 @@
+#Aplicar docstring en las funciones
+#Aplicar funciones para descomponer en partes 
+#Aplicar try-except para manejar errores con los datos ingresados
+#Aplicar importar módulo estándar datetime al agregar un producto al carrito 🛒 
+#Usar una función para calcular la cantidad del producto elegido por su precio 
+#Usar una función para mostrar el total a pagar 
+
+
+# Falla al calcular el total a pagar. No logro sumar los precios obtenidos
+
+
+#Lista principal donde se almacenan los productos
+productos = []
+
+precios = []
+#Módulo datetime 
+from datetime import datetime
+
+
+#Funciones
+
+def fecha_hora_accion():
+    fecha_hora = datetime.now()
+
+    print(fecha_hora.strftime("%d de %B de %Y"))
+
+
+def agregar_producto():
+            
+  #agregar producto: nombre, precio 
+        
+ 
+    #nombre           
+            nombre = input("\nIngresá el nombre del producto (sólo letras):").strip().lower()
+                
+            if nombre.isdigit() or nombre == "":
+                print("\nDato no válido.")
+                 
+            if nombre not in productos:
+        
+        #precio          
+                precio = input("\nIngresá el precio del producto (sólo números enteros positivos): $").strip()
+                
+                if precio.isdigit() and int(precio) >= 0 and precio != "":
+                    #TRABAJAR CON SUB-LISTA
+                    producto = [nombre, precio]
+                    
+                    #AÑADIR A LISTA PRINCIPAL 
+                    productos.append(producto)
+                    
+                    #AÑADIR A LISTA PARA CALCULAR IMPORTE TOTAL 
+                    precios = []
+                    precios.append(precio)
+                    
+                    #Limpiar sub-lista
+                    #producto.clear()
+                    
+                     
+                
+                else:
+                           
+                    print("\nDato no válido")
+                    
+    
+    
+def mostrar_productos():
+    
+    if productos:
+        #Ordenar lista 
+        productos.sort()
+        print("\n\n---Lista de productos actualizada---")
+        for i, producto in enumerate(productos, start=1):
+            print(f"{i}. Nombre del producto: {producto[0]}.  Precio: ${producto[1]}\n")
+            
+    else:
+        print("La lista de productos está vacía.")
+                       
+    
+         
+        
+        
+        
+        
+def borrar_producto():
+    if len(productos) == 0:
+        print("No hay productos ingresados.")
+    
+    if productos:
+        
+        mostrar_productos()
+       
+        
+        number = input("número del producto a Eliminar:")
+                
+        if not number.isdigit():
+            print("❌ Dato no válido.")
+                     
+                
+        if not number:
+            print("❌ Dato no válido.")
+                          
+                
+        #se imprimen los productos numerados desde el 1, por eso se resta 1 al número seleccionado por el usuario, así va a coincidir con la posición (la posición inicia en cero)
+        posicion_borrar = int(number) - 1
+           
+        #le di un nombre a la acción de borrar para usarla como orientación, así ubico con una posición en esta variable el nombre del producto para imprimirlo por pantalla 
+        sublist_borrar = productos.pop(posicion_borrar)
+                    
+        print (f"El producto {sublist_borrar[0]} fue eliminado.\n")
+
+def buscar_producto():
+    
+    if not productos:
+        print("❌ No hay ningún producto en la lista para calcular el costo.")
+        print("Ingrese la opción del menú para agregar un producto.")
+    
+    find = input("\nProducto que desea comprar: ").strip().lower()  
+              
+    if find.isdigit():
+        print("❌ Dato no válido.")
+                
+    if find.isdigit():
+        print("❌ Dato no válido.")
+
+    for producto in productos:
+        if producto[0] == find:
+            print("🔍 Buscando ...")
+            print("PRODUCTO ENCONTRADO")
+            print(f"Nombre del producto: {producto[0].title()} Precio: ${producto[1]}")
+            
+def calcular_por_producto(price, cantidad):  
+    
+    
+     
+    importe = int(price) * int(cantidad)
+    
+    print(f"El total a pagar es ${importe}")
+ 
+
+def calcular_total():   
+    
+    sumar = []
+    suma = 0
+    #mostrar lista principal ordenada 
+    if productos:
+        #Ordenar lista 
+        productos.sort()
+        print("\n\n---Lista de productos actualizada---")
+        for i, producto in enumerate(productos, start=1):
+            print(f"{i}. Nombre del producto: {producto[0]}.  Precio: ${producto[1]}\n")
+            
+        
+            sumar.append(producto[1])
+            print(sumar)
+            
+            for number in sumar:
+                total = suma + int(number)
+                print(total)
+                
+                 
+            
+            """
+        
+            total = sum(sumar)    
+        
+            print(total)
+            print(f"\n\n Cantidad de productos: {len(productos)}")
+            
+            print(f"🛒 Importe total del carrito: ${sumar}")
+            """
+    else:
+        print("La lista de productos está vacía.")
+                       
+    
+    
+      
+             
+    
+            
+            
+def mostrar_menu():  
+    bucle = True                            
+    while bucle == True:
+         print("Menú de gestión compra 🛒:")
+         print("1. Agregar un producto")
+         print("2. Consultar la lista de productos")
+         print("3. Borrar un producto")
+         print("4. Importe total por producto")
+         print("5. Total a pagar")
+         print("6. Salir")
+         
+         opcion = input("Elige una opción del 1 al 6: ").strip()
+         
+         if opcion.isdigit():
+             if 0 < int(opcion) <7:
+                 
+                 if int(opcion) == 1:
+                   producto = agregar_producto()
+                   print("\n\n")
+                   tiempo= fecha_hora_accion()
+                   print("PRODUCTO AGREGADO")
+                   
+                   
+                   print("\n\n---Lista de productos actualizada---")
+                   for i, producto in enumerate(productos, start=1):
+                       print(f"{i}. Nombre del producto: {producto[0]}.  Precio: ${producto[1]}\n")
+            
+                 
+                 elif int(opcion) == 2:
+                   print("\n\n")
+                   tiempo= fecha_hora_accion()
+                   lista = mostrar_productos()
+                   print("\n\n")
+                   
+                 elif int(opcion) == 3:
+                   print("\n\n")
+                   tiempo= fecha_hora_accion()
+                   producto_eliminado = borrar_producto()
+                   print("\n\n")
+                   
+                 
+                 elif int(opcion) == 4:
+                   print("\n\n")
+                   tiempo= fecha_hora_accion()
+                   buscar_producto()
+                   price = input("Ingrese el precio del producto: ").strip()
+    
+    
+                   cantidad = input("Ingrese la cantidad que desea comprar: ").strip()              
+                
+                   calcular_por_producto(price, cantidad)
+                   
+                 elif int(opcion) == 5:
+                   print("\n\n")
+                   tiempo= fecha_hora_accion()   
+                   calcular_total()
+                   
+                 elif int(opcion) == 6:
+                   print("\n\n")
+                   print("Gracias por usar el programa. Chau.")
+                   tiempo = fecha_hora_accion()
+                   bucle = False
+         else: 
+             print("Opción no válida. Por favor, ingrese un número del 1 al 4 (incluidos).\n")
+             
+mostrar_menu()                 
+
+
